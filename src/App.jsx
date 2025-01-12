442553d15c26aad8472a6b7d18ebc8368c7f1f5a
Aniketikhar <aniketikhar88@gmail.com>
2025-01-12 14:42:33 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Category from './pages/Category'
 import Details from './pages/Details'
+import Search from './pages/Search'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='bg-gray-900'>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/:category" element={<Category />} />
-        <Route path="/:id" element={<Details />} />
+        <Route path="/category/:category" element={<Category />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
-    </>
+    </div>
   )
 }
 
